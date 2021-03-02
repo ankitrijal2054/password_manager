4bac893de9e8ec512c88b0e4b42b7e6a97c5c7f8
ankitrijal2054 <ankitrijal2054@gmail.com>
2021-03-02 13:29:59 -0600

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+from tkinter import*
+# ---------------------------- PASSWORD GENERATOR ------------------------------- #
+
+# ---------------------------- SAVE PASSWORD ------------------------------- #
+
+# ---------------------------- UI SETUP ------------------------------- #
+window = Tk()
+window.title("Password Manager")
+window.config(padx=20, pady=20)
+canvas = Canvas(width=200, height=200, highlightthickness=0)
+logo_img = PhotoImage(file="logo.png")
+canvas.create_image(100, 100, image=logo_img)
+canvas.pack()
+
+#creating all label
+web_label = Label(text="Website:", font=("Arial", 24, "bold"))
+
+window.mainloop()
